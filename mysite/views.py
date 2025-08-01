75b759419f71e741196973d6a3fb36281dffdaf6
PDK1395 <pedramgalaxyplayer@gmail.com>
2025-08-01 21:31:23 +0330

    create app and cleaned it  with branches
    we made a app with a branches like about , contact , home  that are for making the site more clear and usable so if we do not put any 'home' like this one in the star branch will be empty and works and that is how we made the site come up on the no /website/ like and just nothing also home is at nothing

--- mysite/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse , JsonResponse
-def http_test(request):
-    return HttpResponse('<h1>hello welcome to my site</h1>')
-
-def json_test(request):
-    return JsonResponse({'name' : 'ali' })
