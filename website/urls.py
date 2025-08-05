be8605c29f05208d24939f301986ad8b8dcfa757
PDK1395 <pedramgalaxyplayer@gmail.com>
2025-08-05 21:22:23 +0330

    alot of things
    fully fixed blog and all thing are linked to the views also made the top and bottem of the site that are the same be a extends inside the site and need to lead the static inside them all all fixed

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,15 @@
 
 from django.urls import path
-from website.views import home_view , about_view , contact_view
+from website.views import home_view , about_view , contact_view , blog_view , blog_home_view , blog_single_view
+
+app_name = 'website'
+
 urlpatterns = [
 
-    path('', home_view),
-    path('about', about_view),
-    path('contact', contact_view),
+    path('', home_view , name= 'index'),
+    path('about', about_view , name= 'about'),
+    path('contact', contact_view , name= 'contact'),
+    path('blog' , blog_view , name= 'blog'),
+    path('blog-home' , blog_home_view , name= 'blog-home'),
+    path('blog-single' , blog_single_view , name= 'blog-single'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse
 def home_view(request):
-    return render(request , 'index.html' )
+    return render(request , 'website/index.html' )
 
 def about_view(request):
-    return render(request , 'about.html' )
+    return render(request , 'website/about.html' )
 
 def contact_view(request):
-    return render(request , 'contact.html' )
+    return render(request , 'website/contact.html' )
+
+def blog_view(request):
+    return render(request , '' )
+
+def blog_home_view(request):
+    return render(request , 'blog/blog-home.html' )
+
+def blog_single_view(request):
+    return render(request , 'blog/blog-single.html' )
+
