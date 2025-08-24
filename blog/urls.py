d874e2900aed4d069c066d08ac0120376ec4d773
PDK1395 <pedramgalaxyplayer@gmail.com>
2025-08-24 09:34:13 +0330

    changed to dynamic
    made posts to be dynamic when for the time we wana show posts inside the blog

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('home' , blog_home_view , name= 'home'),
     path('single' , blog_single_view , name= 'single'),
+    path('post-<int:pid>' , test , name= 'test'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , get_object_or_404
+from blog.models import Post
 def blog_home_view(request):
-    return render(request , 'blog/blog-home.html' )
+    posts = Post.objects.filter(status =1)
+    context = {'posts' : posts}
+    return render(request , 'blog/blog-home.html' , context)
 
 def blog_single_view(request):
     context = {'title':'wow i changed this title with dynamic url' , 'content':'wow omg i changed the body or bottem of this site how cool this is hmmm.' , 'author':'pedram moz'}
     return render(request , 'blog/blog-single.html' , context )
 
+def test(request,pid):
+#    post = Post.objects.get(id=pid)
+    post = get_object_or_404(Post,pk=pid)
+    context = {'pid':pid}
+    return render(request , 'test.html' , context)
