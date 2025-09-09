d55c0f2fa7f5d92e084e808db95dcae60b98960b
PDK1395 <pedramgalaxyplayer@gmail.com>
2025-09-09 19:16:32 +0330

    add category filter

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from blog.models import Post
+from blog.models import Category
+
 register = template.Library()
 
 @register.simple_tag(name='total_posts')
@@ -27,3 +29,12 @@ def snippet(value):
 def latest_posts(arg=2):
     posts = Post.objects.filter(status = 1).order_by('published_date')[:arg]
     return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def post_categories():
+    posts = Post.objects.filter(status = 1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories' : cat_dict}
--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('' , blog_home_view , name= 'home'),
     path('<int:pid>' , blog_single_view , name= 'single'),
+    path('category/<str:cat_name>' , blog_category , name= 'category'),
     path('test', test , name='test')
 ]
 
