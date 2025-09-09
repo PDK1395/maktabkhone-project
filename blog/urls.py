1efcb8584cf38220cbd9d50f1b284f6d55bf5f69
PDK1395 <pedramgalaxyplayer@gmail.com>
2025-09-09 20:12:11 +0330

    filtered it depending on author

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,8 @@ app_name = 'blog'
 urlpatterns = [
     path('' , blog_home_view , name= 'home'),
     path('<int:pid>' , blog_single_view , name= 'single'),
-    path('category/<str:cat_name>' , blog_category , name= 'category'),
+    path('category/<str:cat_name>' , blog_home_view , name= 'category'),
+    path('author/<str:auth_name>' , blog_home_view , name= 'author'),
     path('test', test , name='test')
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,31 @@ from django.shortcuts import render , get_object_or_404
 from blog.models import Post
 from django.utils import timezone
 
-def blog_home_view(request):
+def blog_home_view(request,cat_name=None,auth_name = None):
     posts = Post.objects.filter(published_date__lte = timezone.now() , status =1)
+    
+    if cat_name:
+        posts = posts.filter(category__name = cat_name)
+    if auth_name:
+        posts = posts.filter(author__username = auth_name)
+        
     context = {'posts' : posts}
     return render(request , 'blog/blog-home.html' , context)
+
+
+# in zir rah dovome hamin balayy hast ke age dost dashtim va benazaremon behtar bood mitoni onjory ham benevisim
+'''
+def blog_home_view(request,**kwargs):
+    posts = Post.objects.filter(published_date__lte = timezone.now() , status =1)
+    
+    if kwargs.get('cat_name'):
+        posts = posts.filter(category__name = kwargs['cat_name'])
+    if kwargs.get('auth_name'):
+        posts = posts.filter(author__username = kwargs['auth_name'])
+        
+    context = {'posts' : posts}
+    return render(request , 'blog/blog-home.html' , context)
+'''
 
 
 def blog_single_view(request,pid):
