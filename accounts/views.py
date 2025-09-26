1807cbafea25d05b9561724ceb0496c1e2fc3b8c
PDK1395 <pedramgalaxyplayer@gmail.com>
2025-09-27 01:22:22 +0330

    add full login page and template

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render , redirect
+from django.contrib.auth import login , authenticate
+from django.contrib.auth.forms import AuthenticationForm 
 
 def login_view(request):
-    if request.user.is_authenticated:
-        msg = f' user  is authenticated as {request.user.username} '
-    else :
-        msg = ' user is not authenticated '
-    return render(request , 'accounts/login.html' , { 'msg' : msg})
+    if not request.user.is_authenticated:       
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request,data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(request , username=username , password=password )
+                if user is not None:
+                    login(request , user )
+                    return redirect('/')
+        form = AuthenticationForm()
+        context = { 'form' : form }
+        return render(request , 'accounts/login.html', context )
+    else:
+        return redirect('/')
 
 #def logout_view(request):
 #    return render(request , 'accounts/logout.html')
